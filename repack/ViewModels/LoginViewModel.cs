c99db7af897e1c3f847987c5fcb63c552677d3b6
deezus-net <info@deezus.net>
2019-03-13 15:59:51 +0900

    - [fix] login page error

--- repack/ViewModels/LoginViewModel.cs
+++ repack/ViewModels/LoginViewModel.cs
@@ -1,6 +1,6 @@
 namespace repack.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
         public string Id { get; set; }
         public string Password { get; set; }
